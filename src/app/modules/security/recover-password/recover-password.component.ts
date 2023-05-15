0d00f2a0fa5d7c57f413351a63414c771ff56ca7
Jon Vasquez <jhonvasquezz2002@gmail.com>
2023-05-15 00:57:57 -0500

    se integra la recuperacion de contraseña del front con el back

--- src/app/modules/security/recover-password/recover-password.component.ts
+++ src/app/modules/security/recover-password/recover-password.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SecurityService } from 'src/app/services/security.service';
 
 @Component({
   selector: 'app-recover-password',
   templateUrl: './recover-password.component.html',
-  styleUrls: ['./recover-password.component.css']
+  styleUrls: ['./recover-password.component.css'],
 })
 export class RecoverPasswordComponent {
+  fGroup: FormGroup = new FormGroup({});
 
+  constructor(
+    private fb: FormBuilder,
+    private servicioSeguridad: SecurityService
+  ) {}
+
+  ngOnInit() {
+    this.buildForm();
+  }
+  buildForm() {
+    this.fGroup = this.fb.group({
+      user: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  recuperarClave() {
+    if (this.fGroup.invalid) {
+      alert('debe ingresar un correo valido');
+    } else {
+      let usuario = this.getFormGroup['user'].value;
+      this.servicioSeguridad.RecuperarClavePorUsuario(usuario).subscribe({
+        next: (data) => {
+          alert('se ha enviado una nueva contraseña a su metodo de contacto');
+        },
+        error: (err) => {
+          alert('ha ocurrido un error enviando la contraseña');
+        },
+      });
+    }
+  }
+  get getFormGroup() {
+    return this.fGroup.controls;
+  }
 }
--- src/app/services/security.service.ts
+++ src/app/services/security.service.ts
@@ -6,27 +6,24 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { userValidatedModel } from '../models/user.validated.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-
 export class SecurityService {
-
   urlBase: string = configurationRoutesBackend.urlSecurity;
   constructor(private http: HttpClient) {
     this.validateSesion();
-   }
+  }
 
   /**
    * Identificar usuario
-   * @param usuario 
-   * @param clave 
+   * @param usuario
+   * @param clave
    * @returns datos del usuario validado
    */
   identifyUser(user: string, password: string): Observable<UserModel> {
     return this.http.post<UserModel>(`${this.urlBase}identify-user`, {
       email: user,
-      password: password
+      password: password,
     });
   }
 
@@ -34,40 +31,43 @@ export class SecurityService {
    * Almacena los datos del usuario
    * @param datos datos del usuario
    */
-  storeIdentifiedUserData(data: UserModel): boolean{
+  storeIdentifiedUserData(data: UserModel): boolean {
     let cadena = JSON.stringify(data);
-    let dataLS = localStorage.getItem("data-user");
+    let dataLS = localStorage.getItem('data-user');
     if (dataLS) {
       return false;
     } else {
-      localStorage.setItem("data-user", cadena);
+      localStorage.setItem('data-user', cadena);
       return true;
     }
   }
 
   /**
    * Busca los datos en localstorage de un user
-   * @returns 
+   * @returns
    */
-  getStoredIdentifiedUserData(): UserModel | null{
-    let dataLS = localStorage.getItem("data-user");
+  getStoredIdentifiedUserData(): UserModel | null {
+    let dataLS = localStorage.getItem('data-user');
     if (dataLS) {
       return JSON.parse(dataLS);
-    }else {
+    } else {
       return null;
     }
   }
 
   /**
    * validar codigo 2fa
-   * @param idusuario 
+   * @param idusuario
    * @param codigo
-   * @returns 
+   * @returns
    */
-  validateCode2FA(userId: string, code2fa: string): Observable<userValidatedModel> {
+  validateCode2FA(
+    userId: string,
+    code2fa: string
+  ): Observable<userValidatedModel> {
     return this.http.post<userValidatedModel>(`${this.urlBase}verify-2fa`, {
       userId: userId,
-      code2fa: code2fa
+      code2fa: code2fa,
     });
   }
 
@@ -76,36 +76,43 @@ export class SecurityService {
    * @param data del user validado
    * @returns respuesta
    */
-  storeValidatedUserData(data: userValidatedModel): boolean{
-    let dataLS = localStorage.getItem("data-user-validated");
+  storeValidatedUserData(data: userValidatedModel): boolean {
+    let dataLS = localStorage.getItem('data-user-validated');
     if (dataLS != null) {
       return false;
-    }else{
+    } else {
       let dataString = JSON.stringify(data);
-      localStorage.setItem("data-user-validated", dataString);
+      localStorage.setItem('data-user-validated', dataString);
       return true;
     }
+  }
+
+  RecuperarClavePorUsuario(user: string): Observable<UserModel> {
+    return this.http.post<UserModel>(`${this.urlBase}recovery-password`, {
+      email: user,
+    });
   }
 
   /**
    * administracion de la sesion del user
    */
-  dataUserValidated = new BehaviorSubject<userValidatedModel>(new userValidatedModel());
+  dataUserValidated = new BehaviorSubject<userValidatedModel>(
+    new userValidatedModel()
+  );
 
   getDataSesion(): Observable<userValidatedModel> {
     return this.dataUserValidated.asObservable();
   }
 
-  validateSesion(){
-    let dataLS = localStorage.getItem("data-user-validated");
+  validateSesion() {
+    let dataLS = localStorage.getItem('data-user-validated');
     if (dataLS) {
       let obUserValidated = JSON.parse(dataLS);
       this.updateBehaviorUser(obUserValidated);
     }
   }
 
-  updateBehaviorUser(data: userValidatedModel){
-    return this.dataUserValidated.next(data)
+  updateBehaviorUser(data: userValidatedModel) {
+    return this.dataUserValidated.next(data);
   }
-  
 }
