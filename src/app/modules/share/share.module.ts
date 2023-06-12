465c3138067d1427f2c2a6130e2f1efefed9e8b9
Jon Vasquez <jhonvasquezz2002@gmail.com>
2023-06-12 16:39:22 -0500

    eliminacion de importacion innecesaria

--- src/app/modules/share/share.module.ts
+++ src/app/modules/share/share.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ShareRoutingModule } from "./share-routing.module";
 import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
-import { RedirectTemplateComponent } from './redirect-template/redirect-template.component';
+
 
 @NgModule({
   declarations: [
     ConfirmModalComponent,
-    RedirectTemplateComponent
+   
   ],
   imports: [CommonModule, ShareRoutingModule],
   exports: [ConfirmModalComponent],
